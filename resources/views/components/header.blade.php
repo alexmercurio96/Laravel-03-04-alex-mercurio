4211057d08654b608742993ce74fbfadbbbf0a7c
alexmercurio96 <alexmercurio23@gmail.com>
2023-12-08 21:00:57 +0100

    laravel-03

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,24 @@
+
+{{-- HEADER HOMEPAGE --}}
+
+<div class="container my-5" data-bs-theme="dark">
+
+    <div class="p-5 text-center bg-body-tertiary rounded-3">
+      <svg class="bi mt-4 mb-3" style="color: var(--bs-indigo);" width="100" height="100"><use xlink:href="#bootstrap"></use></svg>
+      <h1 class="text-body-emphasis">THE BABI CINEMA</h1>
+      <p class="col-lg-8 mx-auto fs-5 text-muted">
+        Porca Putténa! Questo pelo irto e setoso mica è fecile; terrone maledetto mi sono sputato in faccia da solo. C'aveva la centrale del Lecce non ho afferreto; perdere e perderemo.
+      </p>
+      <div class="d-inline-flex gap-2 mb-5">
+          
+          <button class="d-flex  btn btn-primary btn-lg rounded-pill"  type="button">
+         
+          <a class="nav-link align-items-center" href="{{route('moviesIndex')}}">i nostri film</a>
+          <svg class="bi ms-2" width="24" height="24"><use xlink:href="#arrow-right-short"></use></svg>
+        </button>
+        <button class="btn btn-outline-secondary btn-lg px-4 rounded-pill" type="button">
+          Secondary link
+        </button>
+      </div>
+    </div>
+  </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,76 +1,10 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
 
-  {{-- NAVBAR--}}
-
-  <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('moviesIndex')}}">Film</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
-
- {{-- FINE NAVBAR --}}
-
- {{-- MUSTHEAD --}}
-
- <div class="container my-5" data-bs-theme="dark">
-    <div class="p-5 text-center bg-body-tertiary rounded-3">
-      <svg class="bi mt-4 mb-3" style="color: var(--bs-indigo);" width="100" height="100"><use xlink:href="#bootstrap"></use></svg>
-      <h1 class="text-body-emphasis">THE BABI CINEMA</h1>
-      <p class="col-lg-8 mx-auto fs-5 text-muted">
-        Porca Putténa! Questo pelo irto e setoso mica è fecile; terrone maledetto mi sono sputato in faccia da solo. C'aveva la centrale del Lecce non ho afferreto; perdere e perderemo.
-      </p>
-      <div class="d-inline-flex gap-2 mb-5">
-        <button class="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" type="button">
-          Call to action
-          <svg class="bi ms-2" width="24" height="24"><use xlink:href="#arrow-right-short"></use></svg>
-        </button>
-        <button class="btn btn-outline-secondary btn-lg px-4 rounded-pill" type="button">
-          Secondary link
-        </button>
-      </div>
-    </div>
-  </div>
- {{-- FOOTER --}}
-
- <div class="container mt-auto">
-    <footer class="py-3 my-4">
-      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
-      </ul>
-      <p class="text-center text-body-secondary">© 2023 Company, Inc</p>
-    </footer>
-  </div>
+<x-layout>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
-</html>
+
+ <x-header></x-header>
+
+ 
+
+</x-layout>
