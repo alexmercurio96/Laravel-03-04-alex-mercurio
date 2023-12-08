5ec96f6cb4571f678ee1d1d8e2c6686ac49bd3e5
alexmercurio96 <alexmercurio23@gmail.com>
2023-12-08 12:44:03 +0100

    Laravel-03

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public $movies = [
+        [
+          'id' => 1,
+          'title' => 'Halloween',
+          'plot' => 'Uno si è svegliato ed ha iniziato a uccidere gente',
+          'director' => 'David Gordon Green',
+          'genere' => 'Horror',
+          'released_at'=> 2018,
+          'img' => '/img/halloween.jpg'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Due sballati al college',
+            'plot' => 'Dal titolo si dovrebbe capire',
+            'director' => 'Method MAN',
+            'genere' => 'Da ridere',
+            'released_at'=> 2001,
+            'img' => '/img/sballati.jpg'
+          ],
+          [
+            'id' => 3,
+            'title' => 'Hulk',
+            'plot' => 'Uomo verde enorme che spacca tutto',
+            'director' => 'Ang Lee',
+            'genere' => 'Fantasy',
+            'released_at'=> 2011,
+            'img' => '/img/hulk.jpg'
+          ],
+          [
+            'id' => 4,
+            'title' => 'Babi',
+            'plot' => 'Cane che mangia e dorme',
+            'director' => 'Alex mercurio',
+            'genere' => 'Horror',
+            'released_at'=> 2024,
+            'img' => '/img/babi.jpg'
+          ],
+          [
+            'id' => 5,
+            'title' => 'Fantozzi contro tutti',
+            'plot' => 'un uomo fuori dagli schemi',
+            'director' => 'Paolo villaggio',
+            'genere' => 'comico',
+            'released_at'=> 1980,
+            'img' => '/img/fantozzi.jpg'
+          ],
+         
+        ];
+
+
+    public function moviesIndex(){
+     
+      return view('movies-index',['movies'=> $this->movies]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\PublicController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',[PublicController::class, 'homepage'])->name('homepage');
+
+// Rotta per mostrare i miei film
+Route::get('/movies/index',[MovieController::class, 'moviesIndex'])->name('moviesIndex');
+
+
